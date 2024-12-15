5c91b22c07b196492983c706c3f9daa1dbb01f64
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-12-16 05:13:10 +0600

    loading global

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function gloabalLoading() {
+    return (
+        <div className='text-red-400 text-4xl'>
+            Loading...
+        </div>
+    );
+}
+
+export default gloabalLoading;
