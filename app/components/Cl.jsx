500ff04c32ffed74ebcfc92cd3bb56e48cee4f73
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-12-16 06:33:50 +0600

    docs loading

--- app/components/Cl.jsx
+++ app/components/Cl.jsx
@@ -9,7 +9,7 @@ function Cl({ child, rootNode }) {
 
     return (
         <Link
-            className={`text-gray-300 hover:text-green-400 w-full block border-l border-gray-500 pl-4 ${path == `/docs/${rootNode.id}/${child.id}` ? 'border-green-400 text-green-400 ' : ''}`}
+            className={`text-gray-300 hover:text-green-400 w-full block border-l border-gray-700 pl-4 ${path == `/docs/${rootNode.id}/${child.id}` ? 'border-l-green-400 text-green-400 ' : ''}`}
             href={`/docs/${rootNode.id}/${child.id}`}
         >{child.title}</Link>
     );
---
+++ app/docs/loading.js
@@ -0,0 +1,10 @@
+"use client"
+function DocsLoading(props) {
+    return (
+        <div className="text-blue-400 text-center mt-20 text-3xl animate-pulse">
+            Loading Docs...
+        </div>
+    );
+}
+
+export default DocsLoading;
